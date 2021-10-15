486734feccd0ef11a2e1d70be3191ee37d556621
Herul Syahwandi S <34206362+galontong@users.noreply.github.com>
2021-10-15 10:51:14 +0700

    Update Program.cs

--- BasicGitMCC58/BasicGitMCC58/Program.cs
+++ BasicGitMCC58/BasicGitMCC58/Program.cs
@@ -10,5 +10,75 @@ namespace BasicGitMCC58
             Console.WriteLine("Dari Herul Syahwandi");
             Console.WriteLine("Afif : Coba GitHub");    
         }
+
+        public void Abay()
+        {
+
+        }
+
+        public void Adzie()
+        {
+
+        }
+        
+        public void Afif()
+        {
+
+        }
+
+        public void Buala()
+        {
+
+        }
+
+        public void Christian()
+        {
+
+        }
+
+        public void Cindy()
+        {
+
+        }
+
+        public void Dio()
+        {
+
+        }
+
+        public void Giri()
+        {
+
+        }
+
+        public void Hamid()
+        {
+
+        }
+
+        public void Leny()
+        {
+
+        }
+
+        public void Lisza()
+        {
+
+        }
+
+        public void Maulana()
+        {
+
+        }
+
+        public void Sharon()
+        {
+
+        }
+
+        public void Syauqi()
+        {
+
+        }
     }
 }
