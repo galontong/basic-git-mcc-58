599aa9035f0ab788a333ecef48cf06ac6a63df17
Herul Syahwandi S <34206362+galontong@users.noreply.github.com>
2021-10-15 10:47:50 +0700

    Update Program.cs

--- BasicGitMCC58/BasicGitMCC58/Program.cs
+++ BasicGitMCC58/BasicGitMCC58/Program.cs
@@ -9,6 +9,7 @@ namespace BasicGitMCC58
             Console.WriteLine("Hello World!");
             Console.WriteLine("Dari Herul Syahwandi");
             Console.WriteLine("Afif : Coba GitHub");    
+            Console.WriteLine("dari branch herul");    
         }
     }
 }
