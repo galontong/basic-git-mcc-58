739ff5d6644527ce114245a325de6fb0c3e3689f
LenyMirtasari <lennymirtasari@gmail.com>
2021-10-15 10:38:42 +0700

    coba push
    coba

--- BasicGitMCC58/BasicGitMCC58/Program.cs
+++ BasicGitMCC58/BasicGitMCC58/Program.cs
@@ -7,6 +7,7 @@ namespace BasicGitMCC58
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("coba push");
         }
     }
 }
